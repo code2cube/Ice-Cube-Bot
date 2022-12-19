b104fa2ef5500fbddd7f222f5086fbe400a2aad2
code2cube <94409128+code2cube@users.noreply.github.com>
2022-12-18 22:08:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import aiosqlite
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
-#https://discord.com/api/oauth2/authorize?client_id=1049077398655742063&permissions=3555978334&scope=bot%20applications.commands
-#https://docs.google.com/spreadsheets/d/1FEL9FeW2PGRX21xia0CyTV4652Wijizq3-KW2PXqyiI/edit#gid=0
+
 load_dotenv()
 
 intents = discord.Intents().all()
